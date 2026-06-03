216bbe07b61e27f1eadac3938e7d31d0aede8373
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 09:39:47 +0400

    Add bookList field to BookController class

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -2,6 +2,13 @@ package com.example.trainee_app;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
+
 @RestController
 public class BookController {
+    private static ArrayList<Book> bookList;
+    
+
+
 }
