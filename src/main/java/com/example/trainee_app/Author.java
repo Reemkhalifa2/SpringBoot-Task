20efed79d1230854291323982382582c13a6d850
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 11:23:03 +0400

    Add biography field and constructor to Author class

--- src/main/java/com/example/trainee_app/Author.java
+++ src/main/java/com/example/trainee_app/Author.java
@@ -3,5 +3,12 @@ package com.example.trainee_app;
 public class Author {
     private Integer id;
     private String name;
+
+    public Author(Integer id, String name, String biography) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+    }
+
     private String biography;
 }
