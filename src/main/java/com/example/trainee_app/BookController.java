b534b8489ea525e0e6f4b420ec9b1a57fa4eac87
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 09:46:48 +0400

    Initialize bookshelf in BookController

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -9,16 +9,13 @@ import java.util.ArrayList;
 
 @RestController
 public class BookController {
-    private static ArrayList<Book> bookshelf;
+    private static ArrayList<Book> bookshelf = new ArrayList<>();
 
     @GetMapping("/add-book")
     public String addBook(@RequestParam int id,
                           @RequestParam String name) {
-
         Book book = new Book(id, name);
-
         bookshelf.add(book);
-
         return "Book added successfully!";
     }
 
