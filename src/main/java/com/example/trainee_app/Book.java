a00b0ea98486b88db97a45c5430a795ccbbcc701
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 09:36:09 +0400

    Add id and name fields to Book class with constructor

--- src/main/java/com/example/trainee_app/Book.java
+++ src/main/java/com/example/trainee_app/Book.java
@@ -1,4 +1,13 @@
 package com.example.trainee_app;
 
 public class Book {
+    private Integer id;
+
+    public Book(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private String name;
+
 }
