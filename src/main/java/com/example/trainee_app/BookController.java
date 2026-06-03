bbbb158430c1b12ea2cdfd6876a428a3d48f4f7c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 09:43:07 +0400

    Add addBook method to BookController for adding books to bookshelf

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -7,8 +9,19 @@ import java.util.ArrayList;
 
 @RestController
 public class BookController {
-    private static ArrayList<Book> bookList;
-    
+    private static ArrayList<Book> bookshelf;
+
+    @GetMapping("/add-book")
+    public String addBook(@RequestParam int id,
+                          @RequestParam String name) {
+
+        Book book = new Book(id, name);
+
+        bookshelf.add(book);
+
+        return "Book added successfully!";
+    }
+
 
 
 }
