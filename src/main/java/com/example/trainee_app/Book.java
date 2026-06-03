05dd02ee8782631d825a4baf6378d2b3d7438fc0
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-03 09:36:38 +0400

    Add getter and setter methods for id and name fields in Book class

--- src/main/java/com/example/trainee_app/Book.java
+++ src/main/java/com/example/trainee_app/Book.java
@@ -3,11 +3,28 @@ package com.example.trainee_app;
 public class Book {
     private Integer id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String name;
     public Book(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    private String name;
+
 
 }
